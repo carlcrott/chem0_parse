cdf329c80ecc110a9b9c4c6110072525d84b62dd
“Carl <carlcrott@gmail.com>
2012-03-03 16:50:02 -0500

    UI looks good

--- app/controllers/chemoinformatics_controller.rb
+++ app/controllers/chemoinformatics_controller.rb
@@ -46,34 +46,21 @@ class ChemoinformaticsController < ApplicationController
   # POST /chemoinformatics.xml
   def create
     @chemoinformatic = Chemoinformatic.new(params[:chemoinformatic])
-
-    puts @chemoinformatic.input_process
     
-#    process = "To a solution of 3-bromobenzophenone (1.00 g, 4 mmol) in MeOH (15 mL) was added sodium borohydride (0.3 mL, 8 mmol) portionwise at rt and the suspension was stirred at rt for 1-24 h. The reaction was diluted slowly with water and extracted with CH2Cl2. The organic layer was washed successively with water, brine, dried over Na2SO4, and concentrated to give the title compound as oil (0.8 g, 79%), which was used in the next reaction without further purification. MS (ESI, pos. ion) m/z: 247.1 (M-OH)."
-
-
-
+    # package API call
     uri = URI("http://localhost:3000/parse")
     http = Net::HTTP.new(uri.host, uri.port)
     headers = { 'Process' => @chemoinformatic.input_process }
-
     codified_process = []
 
     http.get(uri.path, headers) do |chunk|
         codified_process << chunk
     end
 
-    puts codified_process.join("\n") # DB this shit!
     @chemoinformatic.output_process = codified_process.join("\n")
-
-
-
-
-
 
     respond_to do |format|
       if @chemoinformatic.save
-
         format.html { redirect_to(@chemoinformatic, :notice => 'Chemoinformatic was successfully created.') }
         format.xml  { render :xml => @chemoinformatic, :status => :created, :location => @chemoinformatic }
       else
@@ -81,12 +68,6 @@ class ChemoinformaticsController < ApplicationController
         format.xml  { render :xml => @chemoinformatic.errors, :status => :unprocessable_entity }
       end
     end
-
-  # send it to show for this instance ... something like:
-  # redirect_to @chemoinformatic
-  # example:
-  # http://ruby.railstutorial.org/book/ruby-on-rails-tutorial#code:signup_flash
-
   end
 
   # PUT /chemoinformatics/1
--- carls_directions.rb
+++ carls_directions.rb
@@ -106,8 +106,14 @@ $ rake db:migrate VERSION=0
 
 
 
-
+# remigrate w the new expanded 
 $ bundle exec rake db:migrate
+
+
+CREATING NEW CHEMOINFORMATIC PROCESSES WORKS!!!!!
+
+"To a solution of 3-bromobenzophenone (1.00 g, 4 mmol) in MeOH (15 mL) was added sodium borohydride (0.3 mL, 8 mmol) portionwise at rt and the suspension was stirred at rt for 1-24 h. The reaction was diluted slowly with water and extracted with CH2Cl2. The organic layer was washed successively with water, brine, dried over Na2SO4, and concentrated to give the title compound as oil (0.8 g, 79%), which was used in the next reaction without further purification. MS (ESI, pos. ion) m/z: 247.1 (M-OH)."
+
 
 
 
