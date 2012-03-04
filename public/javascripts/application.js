ed8fce92c53d2f36d20f7f8e0d42cb5b4532d9ad
“Carl <carlcrott@gmail.com>
2012-03-04 17:26:37 -0500

    added jQuery and styling looks good

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+
+$(function(){
+  $(document).ready(function(){
+    
+  $('#browse_button').click(function (){
+    $('#chemo_index_link').trigger('click'); 
+  });
+
+  $('#chemo_index_link').click(function(){
+    // alert('STFU');
+  })
+
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
