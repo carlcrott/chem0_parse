09b36a0f9966470b00650752929b1063470c1096
“Carl <carlcrott@gmail.com>
2012-03-04 22:38:06 -0500

    index and shows arrange correctly

--- app/controllers/chemoinformatics_controller.rb
+++ app/controllers/chemoinformatics_controller.rb
@@ -57,7 +57,7 @@ class ChemoinformaticsController < ApplicationController
         codified_process << chunk
     end
 
-    @chemoinformatic.output_process = codified_process.join("<br />")
+    @chemoinformatic.output_process = codified_process.last
 
     respond_to do |format|
       if @chemoinformatic.save
