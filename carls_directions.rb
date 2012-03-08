df0ef9ce2c2198591de4964ea1f38408fa29d891
“Carl <carlcrott@gmail.com>
2012-03-08 17:32:47 -0500

    UI tweaks

--- carls_directions.rb
+++ carls_directions.rb
@@ -114,8 +114,11 @@ CREATING NEW CHEMOINFORMATIC PROCESSES WORKS!!!!!
 
 "To a solution of 3-bromobenzophenone (1.00 g, 4 mmol) in MeOH (15 mL) was added sodium borohydride (0.3 mL, 8 mmol) portionwise at rt and the suspension was stirred at rt for 1-24 h. The reaction was diluted slowly with water and extracted with CH2Cl2. The organic layer was washed successively with water, brine, dried over Na2SO4, and concentrated to give the title compound as oil (0.8 g, 79%), which was used in the next reaction without further purification. MS (ESI, pos. ion) m/z: 247.1 (M-OH)."
 
+#http://www.reddit.com/r/chemistry/comments/q0luy/wannabe_chemhacker_looking_for_unparsed/
 
-
+filter out:
+- bad words
+- empty queries
 
 
 
